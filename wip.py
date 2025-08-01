a441133ec177b9914eded58d840d0155576c6fb1
Ivan Kondratyev <ikondratv@gmail.com>
2025-07-31 21:34:45 -0400

    Updated project dependencies and verified behavior processing works

--- wip.py
+++ wip.py
@@ -3,12 +3,12 @@ from sl_shared_assets import ProjectManifest
 from sl_forgery.utils import get_working_directory
 from sl_forgery.processing import process_behavior_data, fetch_remote_project_manifest
 
-target_sessions = ["2025-07-29-12-49-17-013336", "2025-07-30-12-14-37-233207", "2025-07-31-12-28-21-378576"]
+# target_sessions = ["2025-07-29-12-49-17-013336", "2025-07-30-12-14-37-233207", "2025-07-31-12-28-21-378576"]
 
-process_behavior_data(project="MaalstroomicFlow", sessions=target_sessions, update_manifest=False)
+process_behavior_data(project="MaalstroomicFlow", sessions=None, update_manifest=False, reprocess=True)
 
 fetch_remote_project_manifest(project="MaalstroomicFlow")
 
 target = get_working_directory().joinpath("MaalstroomicFlow", "manifest.feather")
 manifest = ProjectManifest(manifest_file=target)
-manifest.print_summary(15)
+manifest.print_summary(11)
