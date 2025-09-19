9b765155cb2ad23b4653f0361158341303b3de94
Inkaros <ikondratv@gmail.com>
2025-09-19 11:09:45 -0400

    Added suite2p multi-day pipeline constructor code

--- src/sl_forgery/dataset/suite2p.py
+++ src/sl_forgery/dataset/suite2p.py
@@ -1,41 +1,138 @@
 from pathlib import Path
-import shutil as sh
+
+from sl_shared_assets import (
+    Job,
+    Server,
+    ProcessingStatus,
+    ProcessingPipeline,
+    ProcessingPipelines,
+    get_working_directory,
+    TrackerFileNames
+)
+
+from ..utils import get_remote_job_work_directory
 
 
-def collect_single_day_data(source_root: Path, destination_root: Path) -> None:
-    combined_path = source_root / "combined"
+def _construct_suite2p_multiday_pipeline(
+    project: str,
+    animal: int,
+    sessions: list[str],
+    server: Server,
+    manager_id: int,
+    dataset_name: str,
+    configuration_file: str = "GCaMP6f_CA1_MD.yaml",
+    reset_tracker: bool = False,
+    keep_job_logs: bool = False,
+) -> ProcessingPipeline | None:
+    """Generates and returns the ProcessingPipeline instance used to execute the multi-day suite2p processing pipeline
+    for the target set of sessions.
 
-    files = {
-        combined_path / "F.npy",
-        combined_path / "Fneu.npy",
-        combined_path / "Fsub.npy",
-        combined_path / "iscell.npy",
-        combined_path / "ops.npy",
-        combined_path / "spks.npy",
-        combined_path / "stat.npy",
-        source_root / "single_day_ss2p_configuration.yaml"
-    }
+    Notes:
+        Unlike processing pipeline constructors used in the 'processing' package, this constructor expects the input
+        sessions to be pre-validated and always assumes all input sessions can be processed.
 
-    for file in files:
-        sh.copy2(src=file, dst=destination_root.joinpath(file.name))
+    Args:
+        project: The name of the project for which to execute the target processing pipeline.
+        animal: The unique identifier of the animal that performed the sessions processed by this pipeline.
+        sessions: The list of session names to process using the target processing pipeline.
+        server: The Server class instance that manages access to the remote server that executes the pipeline and
+            stores the target session's data.
+        manager_id: The unique identifier of the process that calls this function to construct the pipeline.
+        configuration_file: The name of the configuration file stored on the remote compute server that contains the
+            data-specific processing parameters for the sl-suite2p multi-day pipeline.
+        reset_tracker: Determines whether to reset the processing tracker for the pipeline before executing the
+            processing. This option should only be enabled when recovering from improper runtime terminations.
+        keep_job_logs: Determines whether to keep completed job logs on the server or (default) remove them after
+            runtime. If any pipeline job fails, the logs for all jobs are kept regardless of this argument's
+            value.
 
+    Returns:
+        The configured ProcessingPipeline instance.
+    """
 
-def collect_multi_day_data(source_root: Path, destination_root: Path) -> None:
-    files = {
-        source_root / "F.npy",
-        source_root / "Fneu.npy",
-        source_root / "Fsub.npy",
-        source_root / "ops.npy",
-        source_root / "spks.npy",
-        source_root / "original_images.npy",
-        source_root / "transformed_images.npy",
-        source_root / "unregistered_masks.npy",
-        source_root / "registered_masks.npy",
-        source_root / "shared_multiday_masks.npy",
-        source_root / "session_multiday_masks.npy",
-        source_root / "single_day_ss2p_configuration.yaml",
-        source_root.parent / "multi_day_ss2p_configuration.yaml"
-    }
+    # Resolves the path to the local Sun lab working directory
+    local_working_directory = get_working_directory()
 
-    for file in files:
-        sh.copy2(src=file, dst=destination_root.joinpath(file.name))
+    # Constructs the list of session paths to use int he multiday processing command.
+    session_command = ""
+    for session in sessions:
+        session_path = server.raw_data_root.joinpath(project, str(animal), session)
+        session_command += f"-sp {session_path} "
+
+    # Resolves additional shared flags for the processing CLI.
+    tracker_command = ""
+    if reset_tracker:
+        tracker_command = "-r"
+    configuration_command = f"-i {server.suite2p_configurations_directory.joinpath(configuration_file)}"
+    job_command = f"-j {len(sessions) + 1}"  # Static +1 is for the multi-day cell discovery step.
+
+    # Precreates the iterables to store stage jobs
+    stage_1 = []
+    stage_2 = []
+
+    # Stage 1: Discovery
+    job_name = f"{dataset_name}_ss2p_discovery"
+    working_directory = get_remote_job_work_directory(server=server, job_name=job_name)
+    job = Job(
+        job_name=job_name,
+        output_log=working_directory.joinpath(f"output.txt"),
+        error_log=working_directory.joinpath(f"errors.txt"),
+        working_directory=working_directory,
+        conda_environment="suite2p",
+        cpus_to_use=30,
+        ram_gb=80,
+        time_limit=180,
+    )
+    job.add_command(
+        f"ss2p run {configuration_command} -w -1 sl-multi-day {session_command} "
+        f"-pdr {server.processed_data_root} -id {manager_id} {job_command} {tracker_command} -o "
+        f"{server.user_working_root.joinpath(dataset_name)} -d"
+    )
+    stage_1.append((job, working_directory))
+
+    # Stage 2: Session data extraction.
+    for session in sessions:
+        job_name = f"{dataset_name}_ss2p_session_{session}"
+        working_directory = get_remote_job_work_directory(server=server, job_name=job_name)
+        server.create_directory(remote_path=working_directory)
+        job = Job(
+            job_name=job_name,
+            output_log=working_directory.joinpath(f"output.txt"),
+            error_log=working_directory.joinpath(f"errors.txt"),
+            working_directory=working_directory,
+            conda_environment="suite2p",
+            cpus_to_use=30,
+            ram_gb=80,
+            time_limit=180,
+        )
+        job.add_command(
+            f"ss2p run {configuration_command} -w -1 sl-multi-day {session_command} "
+            f"-pdr {server.processed_data_root} -id {manager_id} {job_command} {tracker_command} -o "
+            f"{server.user_working_root.joinpath(project, dataset_name)} -e -t {session}"
+        )
+        stage_2.append((job, working_directory))
+
+    # Resolves the paths to the local and remote job tracker files.
+    remote_tracker_path = Path(server.user_working_root).joinpath(
+        project, dataset_name, str(animal), TrackerFileNames.MULTIDAY
+    )
+    local_tracker_path = local_working_directory.joinpath(
+        project, dataset_name, str(animal), TrackerFileNames.MULTIDAY
+    )
+
+    # Packages job data into a ProcessingPipeline object and returns it to the caller.
+    pipeline = ProcessingPipeline(
+        jobs={1: tuple(stage_1), 2: tuple(stage_2)},
+        server=server,
+        manager_id=manager_id,
+        pipeline_type=ProcessingPipelines.SUITE2P,
+        remote_tracker_path=remote_tracker_path,
+        local_tracker_path=local_tracker_path,
+        session=dataset_name,
+        animal=str(animal),
+        project=project,
+        keep_job_logs=keep_job_logs,
+        pipeline_status=ProcessingStatus.RUNNING,
+    )
+
+    return pipeline
--- wip.py
+++ wip.py
@@ -2,14 +2,14 @@ from sl_forgery.processing import process_project_data
 
 process_project_data(
     project="StateSpaceOdyssey",
-    # sessions=("2025-07-14-13-49-04-018601",),
-    update_manifest=True,
-    force_lock=True,
+    # sessions=("2025-08-13-16-50-55-726851", "2025-09-08-19-00-55-267485"),
+    update_manifest=False,
+    force_lock=False,
     reprocess=True,
     process_checksum=False,
     recalculate_checksum=False,
     prepare_sessions=False,
-    process_behavior=False,
-    process_suite2p=True,
-    reset_trackers=True,
+    process_behavior=True,
+    process_suite2p=False,
+    reset_trackers=False,
 )
